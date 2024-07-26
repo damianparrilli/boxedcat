acbb1530485cfb48608dce45faf9075a60f6db9e
Damián Parrilli <125889767+damiparrilli@users.noreply.github.com>
2024-07-26 20:40:04 -0300

    Starrate interaction done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-function expandir(event){
+function expandir(event) {
     event.preventDefault();
-            const anchor = event.currentTarget;
-            const contenido = anchor.nextElementSibling;
-            const flecha = anchor.querySelector('.flecha');
-            if ((contenido.style.display === 'none' || contenido.style.display === '') && flecha.classList.contains("active") === false) {
-                contenido.style.display = 'flex';
-                flecha.classList.add('active');
-            } else {
-                contenido.style.display = 'none';
-                flecha.classList.remove('active');
-            }
-    
+    const anchor = event.currentTarget;
+    const contenido = anchor.nextElementSibling;
+    const flecha = anchor.querySelector('.flecha');
+    if ((contenido.style.display === 'none' || contenido.style.display === '') && flecha.classList.contains("active") === false) {
+        contenido.style.display = 'flex';
+        flecha.classList.add('active');
+    } else {
+        contenido.style.display = 'none';
+        flecha.classList.remove('active');
+    }
+
 }
 
 let expando = false;
 
-function expandirTotal(){
+function expandirTotal() {
     expando = !expando;
     const nota = document.querySelector('.contenido-nota');
     const gradiente = nota.lastElementChild;
@@ -23,8 +23,8 @@ function expandirTotal(){
     const verMas = document.querySelector('.ver-mas');
     const verMasImg = verMas.firstElementChild;
     const verMasText = verMas.lastElementChild;
-    
-    if(expando){
+
+    if (expando) {
         nota.style.maxHeight = "100%";
         gradiente.style.visibility = "hidden";
         verMasImg.classList.add("active");
@@ -36,3 +36,60 @@ function expandirTotal(){
         verMasText.textContent = "Ver más";
     }
 }
+
+const estrellas = document.querySelectorAll('.star-rate');
+let seleccionado = false;
+let relleno;
+const numTotal = document.getElementById("numero-total");
+let numeroTotal = parseInt(numTotal.textContent);
+
+console.log(numeroTotal);
+
+estrellas.forEach((estrella, index) => {
+
+    estrella.addEventListener("mouseover", () => {
+        for (let i = 0; i <= index; i++) {
+            estrellas[i].style.fill = "#2CA9F0";
+        }
+
+        for (let i = index + 1; i < estrellas.length; i++) {
+            estrellas[i].style.fill = "transparent";
+        }
+    });
+
+    estrella.addEventListener("click", () => {
+        seleccionado = true;
+        for (let i = 0; i <= index; i++) {
+            estrellas[i].style.fill = "#2CA9F0";
+            relleno = i;
+        }
+
+        for (let i = index + 1; i < estrellas.length; i++) {
+            estrellas[i].style.fill = "transparent";
+        }
+
+        numTotal.textContent = `${numeroTotal + 1}`;
+    });
+
+    estrella.addEventListener("mouseout", () => {
+        if (!seleccionado) {
+            for (let i = 0; i < estrellas.length; i++) {
+
+                estrellas[i].style.fill = "transparent";
+            }
+        } else {
+            for (let i = 0; i <= relleno; i++) {
+                estrellas[i].style.fill = "#2CA9F0";
+            }
+            
+            for (let i = relleno + 1; i < estrellas.length; i++) {
+                estrellas[i].style.fill = "transparent";
+            }
+        }
+    }
+    );
+
+
+
+});
+
