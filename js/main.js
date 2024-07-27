2ac5c6f591485790c110bfc4d4791c3660da266b
Damián Parrilli <125889767+damiparrilli@users.noreply.github.com>
2024-07-27 15:00:31 -0300

    Comments section almost done

--- js/main.js
+++ js/main.js
@@ -43,7 +43,6 @@ let relleno;
 const numTotal = document.getElementById("numero-total");
 let numeroTotal = parseInt(numTotal.textContent);
 
-console.log(numeroTotal);
 
 estrellas.forEach((estrella, index) => {
 
@@ -81,7 +80,7 @@ estrellas.forEach((estrella, index) => {
             for (let i = 0; i <= relleno; i++) {
                 estrellas[i].style.fill = "#2CA9F0";
             }
-            
+
             for (let i = relleno + 1; i < estrellas.length; i++) {
                 estrellas[i].style.fill = "transparent";
             }
@@ -92,4 +91,118 @@ estrellas.forEach((estrella, index) => {
 
 
 });
+
+let usuario = "Damian";
+const formComentario = document.getElementById("formComentario");
+const textarea = document.getElementById("comentario");
+const divComentarios = document.querySelector(".comentarios-escritos");
+let mostrameMas = false;
+let comentarios = document.querySelectorAll(".comentarios-escritos > :not(:first-child)");
+
+formComentario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (textarea.value !== "") {
+        const cantidadComentarios = document.getElementById("cantidad-comentarios");
+        let cantTotal = parseInt(cantidadComentarios.textContent);
+        cantidadComentarios.textContent = `${cantTotal + 1}`;
+        const fechaActual = new Date();
+
+        const opcionesDeFecha = {
+            timeZone: 'America/Argentina/Buenos_Aires',
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        };
+
+        const formato = new Intl.DateTimeFormat('es-ES', opcionesDeFecha);
+        const partes = formato.formatToParts(fechaActual);
+        const fecha = partes.find(p => p.type === 'day').value + '/' +
+                      partes.find(p => p.type === 'month').value + '/' +
+                      partes.find(p => p.type === 'year').value;
+        const hora = partes.find(p => p.type === 'hour').value + ':' +
+                     partes.find(p => p.type === 'minute').value;
+
+        const comentarioIndividual = document.createElement("div");
+        comentarioIndividual.classList.add("comentario-individual");
+        const imagenPerfil = document.createElement("img");
+        imagenPerfil.src = "./img/autor.svg";
+        imagenPerfil.alt = "Profile Photo";
+        imagenPerfil.classList.add("profile-photo")
+        const nombreYFecha = document.createElement("div");
+        const nombre = document.createElement("h3");
+        nombre.textContent = `${usuario}`;
+        const fechap = document.createElement("p");
+        fechap.textContent = `${fecha} ${hora}`;
+        const comentarioEscrito = document.createElement("p");
+        comentarioEscrito.textContent = `${textarea.value}`;
+        nombreYFecha.append(nombre, fechap);
+        comentarioIndividual.append(imagenPerfil, nombreYFecha, comentarioEscrito);
+        divComentarios.insertBefore(comentarioIndividual, divComentarios.firstChild);
+        comentarios = document.querySelectorAll(".comentarios-escritos > :not(:first-child)");
+        if(mostrameMas == false){
+            comentarios.forEach(comentario => {
+                comentario.style.display = 'none';
+            });
+        }
+        textarea.value = "";
+    }
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    comentarios.forEach(comentario => {
+        comentario.style.display = 'none';
+    });
+
+});
+
+
+function mostrarMas() {
+
+    
+    const btnMostrarMas = document.getElementById("mostrar-mas");
+    const styleSheet = document.styleSheets[0];
+    const rules = styleSheet.cssRules;
+
+    if (mostrameMas == false) {
+
+        for (let i = 0; i < rules.length; i++) {
+            if (rules[i].selectorText === '#mostrar-mas::before') {
+                rules[i].style.transform = 'rotate(180deg)';
+                rules[i].style.backgroundPosition = "center left 35px";
+            }
+        }
+
+
+        comentarios.forEach(comentario => {
+            comentario.style.display = 'flex';
+        });
+
+        btnMostrarMas.textContent = "Mostrar menos";
+
+    } else {
+
+        for (let i = 0; i < rules.length; i++) {
+            if (rules[i].selectorText === '#mostrar-mas::before') {
+                rules[i].style.transform = 'rotate(0deg)';
+                rules[i].style.backgroundPosition = "center right 35px";
+            }
+        }
+
+
+        comentarios.forEach(comentario => {
+            comentario.style.display = 'none';
+        });
+
+        btnMostrarMas.textContent = "Mostrar más";
+
+    }
+    mostrameMas = !mostrameMas;
+}
 
