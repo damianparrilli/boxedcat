58a5a9612c25b07e1c2b42660eebbd0d2d353fd0
Damián Parrilli <125889767+damiparrilli@users.noreply.github.com>
2024-07-31 18:22:29 -0300

    Register phase 1 completed, 2 phases remaining

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ const bookmarkNota = document.querySelectorAll('.bookmark-nota');
 const divLogin = document.getElementById("login-popup");
 
 
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombres: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
 
 
 function manejadorLogin(funcion) {
@@ -20,8 +27,8 @@ function manejadorLogin(funcion) {
 
 function mostrarFormularioLogin(event) {
 
-    
-   
+
+
     const overlay = document.createElement("div");
     overlay.className = "overlay";
     const contenedor = document.createElement("div");
@@ -32,25 +39,35 @@ function mostrarFormularioLogin(event) {
     logo.className = "logo-login";
     const titulo = document.createElement("h3");
     const formLogin = document.createElement("form");
-    formLogin.className = "formulario-login";
-    const usuario = document.createElement("input");
-    usuario.type = "text";
-    usuario.name = "usuario";
-    usuario.id = "usuario";
-    usuario.placeholder = "Usuario";
-    usuario.required = true;
+    formLogin.className = "formularios-registro";
+    formLogin.id = "formulario-login";
+    const email = document.createElement("input");
+    email.type = "email";
+    email.name = "email";
+    email.id = "email";
+    email.placeholder = "Email";
+    const divContraseña = document.createElement("div");
+    divContraseña.className = "div-contraseña";
     const contraseña = document.createElement("input");
     contraseña.type = "password";
     contraseña.name = "contraseña";
     contraseña.id = "contraseña";
-    contraseña.required = true;
     contraseña.placeholder = "Contraseña";
+    const ojo = document.createElement("img");
+    ojo.src = "./img/closed-eye.svg";
+    const textoError = document.createElement("p");
+    textoError.className = "texto-error";
     const olvidasteContraseña = document.createElement("a");
     olvidasteContraseña.textContent = "¿Olvidaste tu contraseña?";
     const loginBtn = document.createElement("button");
     loginBtn.textContent = "INICIAR SESIÓN";
+    loginBtn.className = "iniciar-sesion-button";
+    const loginBtnAcceso = document.createElement("button");
+    loginBtnAcceso.textContent = "INICIAR SESIÓN";
+    loginBtnAcceso.className = "iniciar-sesion-button";
     const textoEnMedio = document.createElement("p");
     textoEnMedio.textContent = "o";
+    textoEnMedio.className = "separador";
     const contenedorButtons = document.createElement("div");
     contenedorButtons.className = "contenedor-btn-login";
     const googleLogin = document.createElement("button");
@@ -58,6 +75,7 @@ function mostrarFormularioLogin(event) {
     const fbLogin = document.createElement("button");
     fbLogin.textContent = "FACEBOOK";
     const registrate = document.createElement("p");
+    registrate.className = "registrate";
     registrate.textContent = "¿No tenés cuenta? ";
     const registrateAnchor = document.createElement("a");
     registrateAnchor.textContent = "Registrate";
@@ -68,25 +86,182 @@ function mostrarFormularioLogin(event) {
     cerrar.id = "cerrar-form";
     const cerrarimg = document.createElement("img");
     cerrarimg.src = "./img/close.svg";
-    
-    if (event.currentTarget.id == "login-btn") {
-        titulo.textContent = "Inicia sesión en BoxedCat";
-        formLogin.append(usuario, contraseña, olvidasteContraseña);
-    } else {
-        titulo.textContent = "Para continuar debes iniciar sesión";
-    }
+
+
+
     cerrar.append(cerrarimg);
     registrate.appendChild(registrateAnchor);
     contenedorButtons.append(googleLogin, fbLogin);
-    formLogin.append(loginBtn);
-    contenedor.append(logo, titulo, formLogin, textoEnMedio, contenedorButtons, cerrar, registrate);
+    divContraseña.append(contraseña, ojo);
+    contenedor.append(logo, titulo, loginBtnAcceso, textoEnMedio, contenedorButtons, cerrar, registrate);
     overlay.append(contenedor);
     divLogin.append(overlay);
+
+    googleLogin.addEventListener('click', registrarse);
+    fbLogin.addEventListener('click', registrarse);
+    registrate.addEventListener('click', registrarse);
+    ojo.addEventListener('click', function () {
+        let tipo = contraseña.type === "password" ? "text" : "password";
+        contraseña.setAttribute("type", tipo);
+        if (tipo == "password") {
+            ojo.src = "./img/closed-eye.svg";
+        } else {
+            ojo.src = "./img/open-eye.svg";
+        }
+    });
+
+    if (event.currentTarget.id == "login-btn" || event.currentTarget.id == "back") {
+        abrirLogin();
+    } else {
+        titulo.textContent = "Para continuar debes iniciar sesión";
+        loginBtnAcceso.addEventListener('click', abrirLogin);
+    }
+
+    function registrarse() {
+        contenedor.innerHTML = "";
+        abrirFormRegistro(contenedor, logo, titulo, cerrar);
+    };
+
+    function abrirLogin() {
+        titulo.textContent = "Inicia sesión en BoxedCat";
+        loginBtnAcceso.replaceWith(formLogin);
+        formLogin.append(email, divContraseña, textoError, olvidasteContraseña, loginBtn);
+
+        formLogin.addEventListener('submit', function (event) {
+            event.preventDefault();
+            textoError.textContent = "*Email o contraseña invalido";
+        });
+    }
     adjustScale();
 }
 
-function abrirFormRegistro() {
+function abrirFormRegistro(container, imglogo, title, cerra) {
+    const contenedor = container;
+    const logo = imglogo;
+    const titulo = title;
+    const cerrar = cerra;
+    if (contenedor != null) {
+        titulo.textContent = "Registrate para saber de BoxedCat";
 
+        const formRegistro1 = document.createElement("form");
+        formRegistro1.className = "formularios-registro";
+        formRegistro1.id = "formulario-registro";
+        formRegistro1.setAttribute("novalidate", "true");
+
+        const nombre = document.createElement("input");
+        nombre.type = "text";
+        nombre.name = "nombre";
+        nombre.id = "nombre";
+        nombre.placeholder = "Nombre";
+        const textoErrorName = document.createElement("p");
+        textoErrorName.className = "texto-error";
+
+        const email = document.createElement("input");
+        email.type = "email";
+        email.name = "email";
+        email.id = "email";
+        email.placeholder = "Email";
+        const textoErrorEmail = document.createElement("p");
+        textoErrorEmail.className = "texto-error";
+
+        const divContraseña = document.createElement("div");
+        divContraseña.className = "div-contraseña";
+        const contraseña = document.createElement("input");
+        contraseña.type = "password";
+        contraseña.name = "contraseña";
+        contraseña.id = "contraseña";
+        contraseña.placeholder = "Contraseña";
+        const ojo = document.createElement("img");
+        ojo.src = "./img/closed-eye.svg";
+        const textoErrorContraseña = document.createElement("p");
+        textoErrorContraseña.className = "texto-error";
+
+        const confirmarContraseña = document.createElement("input");
+        confirmarContraseña.type = "password";
+        confirmarContraseña.name = "confirmarContraseña";
+        confirmarContraseña.id = "confirmarContraseña";
+        confirmarContraseña.placeholder = "Confirmar contraseña";
+        const textoErrorConfirmarContraseña = document.createElement("p");
+        textoErrorConfirmarContraseña.className = "texto-error";
+
+        const terminosYCondiciones = document.createElement("p");
+        terminosYCondiciones.className = "terminos-y-condiciones";
+        terminosYCondiciones.innerHTML = "Al continuar, indicas que aceptas las <a>Condiciones de servicio</a> y la <a>Politica de privacidad</a> de BoxedCat";
+        const registrarseBtn = document.createElement("button");
+        registrarseBtn.textContent = "REGISTRARSE";
+        registrarseBtn.className = "iniciar-sesion-button";
+        
+        const back = document.createElement("img");
+        back.src = "./img/back.svg";
+        back.id = "back";
+
+        divContraseña.append(contraseña, ojo);
+        formRegistro1.append(nombre, textoErrorName, email, textoErrorEmail, divContraseña, textoErrorContraseña, confirmarContraseña, textoErrorConfirmarContraseña, terminosYCondiciones, registrarseBtn);
+        contenedor.append(logo, titulo, cerrar, back, formRegistro1);
+
+        ojo.addEventListener('click', function () {
+            let tipo = contraseña.type === "password" ? "text" : "password";
+            contraseña.setAttribute("type", tipo);
+            ojo.src = tipo === "password" ? "./img/closed-eye.svg" : "./img/open-eye.svg";
+        });
+
+        formRegistro1.addEventListener('submit', function (event) {
+            event.preventDefault();
+            if(nombre.value == ""){
+                textoErrorName.textContent = "*Este campo no puede estar vacio";
+                textoErrorName.classList.add("active");
+                nombre.classList.add("error");
+            } else if(!expresiones.nombres.test(nombre.value)){
+                textoErrorName.textContent = "*Estas usando caracteres invalidos para este campo";
+                textoErrorName.classList.add("active");
+                nombre.classList.add("error");
+            } else {
+                textoErrorName.textContent = "";
+                textoErrorName.classList.remove("active");
+                nombre.classList.remove("error");
+            }
+
+            if(email.value == ""){
+                textoErrorEmail.textContent = "*Este campo no puede estar vacio";
+                textoErrorEmail.classList.add("active");
+                email.classList.add("error");
+            } else if(!expresiones.correo.test(email.value)){
+                textoErrorEmail.textContent = '*Se requiere un correo con "@" y "."';
+                textoErrorEmail.classList.add("active");
+                email.classList.add("error");
+            } else {
+                textoErrorEmail.textContent = "";
+                textoErrorEmail.classList.remove("active");
+                email.classList.remove("error");
+            }
+
+            if(contraseña.value == ""){
+                textoErrorContraseña.textContent = "*Este campo no puede estar vacio";
+                textoErrorContraseña.classList.add("active");
+                contraseña.classList.add("error");
+            } else {
+                textoErrorContraseña.textContent = "";
+                textoErrorContraseña.classList.remove("active");
+                contraseña.classList.remove("error");
+            }
+
+            if(contraseña.value != "" && confirmarContraseña.value != "" && confirmarContraseña.value === contraseña.value){
+                textoErrorConfirmarContraseña.textContent = "";
+                textoErrorConfirmarContraseña.classList.remove("active");
+                confirmarContraseña.classList.remove("error");
+            } else {
+                textoErrorConfirmarContraseña.textContent = "*Confirmación invalida";
+                textoErrorConfirmarContraseña.classList.add("active");
+                confirmarContraseña.classList.add("error");
+            }
+           
+        });
+
+        back.addEventListener("click", (event) => {
+            document.getElementById("login-popup").innerHTML = "";
+            mostrarFormularioLogin(event);
+        });
+    }
 };
 
 
@@ -327,16 +502,15 @@ function mostrarMas() {
 
 function adjustScale() {
     var viewportHeight = window.innerHeight;
-    var scaleValue = 1; 
-  
+    var scaleValue = 1;
+
     if (viewportHeight < 800) {
-      scaleValue = viewportHeight / 800; 
+        scaleValue = viewportHeight / 800;
     }
-    console.log(viewportHeight)
     var element = document.getElementById('login-container');
     if (element) {
-      element.style.transform = 'scale(' + scaleValue + ')';
+        element.style.transform = 'scale(' + scaleValue + ')';
     }
-  }
-  
-  window.onresize = adjustScale;
+}
+
+window.onresize = adjustScale;
